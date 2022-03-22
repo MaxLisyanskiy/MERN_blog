0897175e99fcf1e75614cd17600fd9350c000100
MaxLisyanskiy <max_lisyanskiy@mail.ru>
2022-03-22 14:10:45 +0300

    finished back-register

--- back/routers/userRoutes.js
+++ back/routers/userRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express'
 
-import { getUserProfile } from '../controllers/userController.js'
+import { getUserProfile } from '../controllers/user/profileController.js'
+import { registerUser } from '../controllers/user/regController.js'
 
 const route = express.Router()
 
 route.route('/profile').get(getUserProfile)
+route.route('/').post(registerUser)
 
 export default route
